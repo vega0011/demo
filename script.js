5a5e89c55cd3dab569c4f3cf182ea8d84ad68e09
gautam thakur <gautamthakur0011@gmail.com>
2024-05-27 10:21:20 +0530

    signup added

---
+++ script.js
@@ -0,0 +1,13 @@
+// Created by Be Alone😎
+
+//created by Be Alone😎
+
+function fun(){
+    Swal.fire("Log in successfull");
+          }
+    function Notavailable(){
+    Swal.fire("Not available");
+          }
+    function ohhno(){
+    Swal.fire("I don't know");
+          }
